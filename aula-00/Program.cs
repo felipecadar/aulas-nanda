23ca26db8ce2827a4c2e7161009805e82455320e
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2022-04-04 23:08:48 -0300

    First Example

---
+++ aula-00/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+Account account = new Account
+{
+    Name = "John Doe",
+    Email = "john@nuget.org",
+    DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+};
+
+Console.WriteLine(account.Name);
+Console.WriteLine(account.Email);
+Console.WriteLine(account.DOB);
+
+public class Account
+{
+    public string? Name { get; set; } // "?" indica que o campo pode ser nulo
+    public string? Email { get; set; }
+    public DateTime? DOB { get; set; }
+}
